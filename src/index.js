68fef01f4101c6b86e24dffdd45be239f0c64831
Blue32A <blue32a.jp@gmail.com>
2021-07-05 23:53:48 +0900

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import ReactDOM from 'react-dom';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const script = '<script>alert(1)</script>';
+
+const element = (
+  <div>
+    <h1 className="greeting">
+      Hello, {formatName(user)}!
+    </h1>
+    <p>{script}</p>
+  </div>
+);
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
