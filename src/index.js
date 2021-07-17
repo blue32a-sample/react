2c653d4bd7df27210ed7fc3ca1c9c3da7e8d28bf
Blue32A <blue32a.jp@gmail.com>
2021-07-17 15:16:13 +0900

    Lists and Keys

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {isToggleOn: true};
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
-    )
-  }
+function ListItem(props) {
+  return <li>{props.value}</li>;
 }
 
+function NumberList(props) {
+  const numbers = props.numbers;
+  return (
+    <ul>
+      {numbers.map((number) =>
+        <ListItem key={number.toString()} value={number} />
+      )}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <Toggle />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
